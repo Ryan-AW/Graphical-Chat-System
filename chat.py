a95b7e69ccf818db32606f4bfcb86bc423ff7c00
Ryan-AW <50891728+Ryan-AW@users.noreply.github.com>
2021-05-07 22:56:38 -0400

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,29 +1,28 @@
+# Standard Libraries
+import socket
+import threading
+import re
+
+# Encryption Library
+from cryptography.fernet import Fernet
+
 # PyQt5 - GUI Library
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
-# Standard Libraries
-import socket
-import threading
-import time
-import re
-
-# ---------------------------- #
-
-# Window Properties
-WindowTitle = "Chat Room"
-BackgroundColour = "black"
-status = "Not Connected"
-pattern = r"[a-zA-Z0-9()$%_/.]*$"
-
-width = 680
-height = 440
+# GUI Properties
+TITLE = "Chat Room"
+BACKGROUND = "black"
+WIDTH = 680
+HEIGHT = 440
 
 # Connection Properties
-ServerIP = "0.0.0.0"
-PORT = 10000
-buffer = 1024
+server = "0.0.0.0"
+connStatus = "Not Connected"
+PORT = 5005
+BUFFER = 1024
+KEY = None
 
 username = None
 objSocket = None
@@ -33,136 +32,119 @@ class ChatWindow(QDialog):
     def __init__(self):
         super(ChatWindow, self).__init__()
 
-        # Disable Maximize Button and Stretch
+        # Disable Maximize Button & Stretch
         self.setWindowFlags(QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowMinimizeButtonHint)
-        self.setFixedSize(width, height)
+        self.setFixedSize(WIDTH, HEIGHT)
 
-        # Display Window Title
-        self.setWindowTitle(WindowTitle)
+        # Title | Icon | Background
+        self.setWindowTitle(TITLE)
         self.setWindowIcon(QtGui.QIcon("images/icon.png"))
-        self.setStyleSheet(f"background: {BackgroundColour}")
+        self.setStyleSheet("background: {}".format(BACKGROUND))
 
-        # Display Server Status
+        # Server Status
         self.StatusLabel = QLabel("Status: ", self); self.StatusLabel.move(5, 10)
         self.StatusLabel.setObjectName("StatusLabel")
 
-        self.ServerStatus = QLabel(f"{status}", self); self.ServerStatus.move(70, 10)
-        self.ServerStatus.setObjectName("ServerStatusLabel")
+        self.ServerStatusLabel = QLabel(f"{connStatus}", self); self.ServerStatusLabel.move(70, 10)
+        self.ServerStatusLabel.setObjectName("ServerStatusLabel")
 
-        # Username Label & Display Username Box
+        # Username Box
         self.UsernameLabel = QLabel("Username: ", self);  self.UsernameLabel.move(5, 40)
         self.UsernameLabel.setObjectName("UsernameLabel")
 
-        self.UsernameInput = QLineEdit(self); self.UsernameInput.setMaxLength(13); self.UsernameInput.move(105, 42)
-        self.UsernameInput.setObjectName("UsernameInput")
+        self.UsernameBox = QLineEdit(self); self.UsernameBox.setMaxLength(13); self.UsernameBox.move(105, 42)
+        self.UsernameBox.setValidator(QtGui.QRegExpValidator(QtCore.QRegExp("[a-zA-Z0-9]+")))
+        self.UsernameBox.setObjectName("UsernameBox")
 
-        # Server IP Label & Display Server IP Box
-        self.ServerIPLabel = QLabel("Server IP: ", self); self.ServerIPLabel.move(5, 70)
-        self.ServerIPLabel.setObjectName("ServerIPLabel")
+        # Server Box
+        self.ServerLabel = QLabel("Server IP: ", self); self.ServerLabel.move(5, 70)
+        self.ServerLabel.setObjectName("ServerLabel")
 
-        self.ServerIPInput = QLineEdit(self); self.ServerIPInput.setMaxLength(15); self.ServerIPInput.move(105, 70)
-        self.ServerIPInput.setObjectName("ServerIPInput")
+        self.ServerBox = QLineEdit(self); self.ServerBox.setMaxLength(15); self.ServerBox.move(105, 70)
+        self.ServerBox.setObjectName("ServerBox")
 
         # Divider Line
         self.DividerLine = QLabel("_"*34, self); self.DividerLine.move(7, 100); self.DividerLine.setStyleSheet("color: white")
 
-        # Connect to Server - Button
+        # Chat Box (mode: readonly)
+        self.ChatBox = QTextEdit(self); self.ChatBox.move(220, 10); self.ChatBox.resize(340, 340)
+        self.ChatBox.setStyleSheet("background: lightgrey"); self.ChatBox.setReadOnly(True); self.ChatBox.setObjectName("ChatBox")
+
+        # Text Box
+        self.TextBox = QTextEdit(self); self.TextBox.move(220, 355); self.TextBox.resize(340, 75)
+        self.TextBox.setStyleSheet("background: white")
+
+        # Send Button
+        self.SendMessage = QPushButton("Send", self); self.SendMessage.move(565, 355); self.SendMessage.resize(110, 75)
+        self.SendMessage.setStyleSheet("background: rgb(45, 45, 236)"); self.SendMessage.setObjectName("SendButton")
+        self.SendMessage.clicked.connect(self.Send)
+
+        # Connect Button
         self.ConnectButton = QPushButton("Connect", self); self.ConnectButton.setStyleSheet("background: grey")
         self.ConnectButton.move(10, 120); self.ConnectButton.setObjectName("ConnectButton")
         self.ConnectButton.clicked.connect(self.ServerConnect)
 
-        # Disconnect from Server - Button
+        # Disconnect Button
         self.DisconnectButton = QPushButton("Disconnect", self); self.DisconnectButton.setStyleSheet("background: grey")
         self.DisconnectButton.move(90, 120); self.DisconnectButton.setObjectName("DisconnectButton")
         self.DisconnectButton.clicked.connect(self.ServerDisconnect)
 
-        # Clear Chat - Button
+        # Encryption Label
+        self.EncryptionLabel = QLabel("🔴 Encryption: Secured", self)
+        self.EncryptionLabel.setObjectName("EncryptionLabel"); self.EncryptionLabel.move(7, 150)
+
+        # Clear Button
         self.ClearButton = QPushButton("Clear Chat", self); self.ClearButton.setStyleSheet("background: grey")
         self.ClearButton.move(565, 10); self.ClearButton.setObjectName("ClearButton")
-        self.ClearButton.clicked.connect(self.ClearChat)
+        self.ClearButton.clicked.connect(lambda:self.ChatBox.setText(None))
 
-        # Chat Box to View Messages
-        self.ChatBox = QTextEdit(self); self.ChatBox.move(220, 10); self.ChatBox.resize(340, 340)
-        self.ChatBox.setStyleSheet("background: lightgrey"); self.ChatBox.setReadOnly(True)
-
-        # Text Box (messages)
-        self.InputBox = QTextEdit(self); self.InputBox.move(220, 355); self.InputBox.resize(340, 75)
-        self.InputBox.setStyleSheet("background: white")
-
-        # Send Message Button
-        self.SendMessage = QPushButton("Send", self); self.SendMessage.move(565, 355); self.SendMessage.resize(110, 75)
-        self.SendMessage.setStyleSheet("background: rgb(45, 45, 236)"); self.SendMessage.setObjectName("SendMessageButton")
-        self.SendMessage.clicked.connect(self.Send)
-
-        # Show Application
         self.show()
 
-    # Username Character(s) error
-    def ErrorMessageBox(self, ErrorMessage):
-        MessageBox = QMessageBox(self)
-        MessageBox.setIcon(QMessageBox.Critical)
-        MessageBox.setWindowTitle("Error")
-        MessageBox.setText(f"{ErrorMessage}\t")
-        MessageBox.setStyleSheet("color: yellow")
-        MessageBox.show()
-
-    # Send Message
-    def Send(self):
-        InputText = self.InputBox.toPlainText()
-        if (InputText.strip() == ""):
-            return
-
-        elif (len(InputText) >= 200):
-            self.ChatBox.append("[-] Maximum of 200 Characters is only Permitted")
-            self.InputBox.setText(None)
-            return
-
-        try:
-            objSocket.send(bytes(InputText, "utf-8"))
-        except:
-            self.ChatBox.setText("<Not Connected to Server>")
-        finally:
-            self.InputBox.setText(None)
-
-    # Disconnect from Server
-    def ServerDisconnect(self):
-        if (objSocket):
-            objSocket.close()
-
-            self.ChatBox.append("<Disconnected from Server>")
-            self.Disconnected()
-        else:
-            self.ChatBox.setText("<Not Connected to Server>")
-
-    # Connect to Server
     def ServerConnect(self):
-        if (self.UsernameInput.text().strip() == ""):
-            self.ErrorMessageBox("Invalid Username")
+        global username, server
+        username = self.UsernameBox.text()
+        server = self.ServerBox.text()
+
+        if (len(self.UsernameBox.text().strip()) == 0 or len(self.ServerBox.text().strip()) == 0):
+            self.ChatBox.append("You must provide a Username/Server IP")
             return
 
-        elif not (re.match(pattern, self.UsernameInput.text())):
-            self.ErrorMessageBox("Invalid Username")
-            return
-        else:
-            global username, ServerIP
-            username = self.UsernameInput.text()
-            ServerIP = self.ServerIPInput.text()
-
-        self.ServerStatus.setText("Connecting...")
+        self.ServerStatusLabel.setText("Connecting...")
         self.ChatBox.setText(None)
 
         client = ClientThread(window)
         client.daemon = True
         client.start()
 
-        self.UsernameInput.setReadOnly(True); self.ServerIPInput.setReadOnly(True)
+        self.UsernameBox.setReadOnly(True); self.ServerBox.setReadOnly(True)
         self.ConnectButton.setEnabled(False)
 
-    # Clear Chat Log
-    def ClearChat(self):
-        self.ChatBox.setText(None)
+    def ServerDisconnect(self):
+        if (objSocket):
+            objSocket.close()
 
-    # Receive Data and Display Messages
+            self.ChatBox.append("You have been disconnected")
+            self.Disconnected()
+        else:
+            self.ChatBox.setText("You are not connected to a Server")
+
+    def Send(self):
+        text = self.TextBox.toPlainText()
+        if (len(text) >= 200):
+            self.ChatBox.append("[-] Maximum of 200 Characters Only")
+            self.TextBox.setText(None)
+            return
+
+        elif (len(text.strip())== 0):
+            return
+
+        try:
+            objSocket.send(Fernet(KEY).encrypt(bytes(text, "utf-8")))
+        except:
+            self.ChatBox.setText("You are not connected to a Server")
+        finally:
+            self.TextBox.setText(None)
+
     def ReceivedMessage(self, data):
         if (re.findall(username, data)):
             data = "You: " + data.split(':')[1]
@@ -174,25 +156,14 @@ class ChatWindow(QDialog):
 
         self.ChatBox.append(f"<span style=color:{colour}>{data}</span>")
 
-    # Display when connected to the server
-    def Connected(self):
-        self.ServerStatus.setText("Connected"); self.ServerStatus.setStyleSheet("color: green")
-
-    # Display when disconnected from the server
     def Disconnected(self):
-        self.ServerStatus.setText(f"{status}")
-        self.ServerStatus.setStyleSheet("color: red")
+        self.ServerStatusLabel.setText(connStatus)
+        self.ServerStatusLabel.setStyleSheet("color: red")
 
-        self.UsernameInput.setReadOnly(False)
-        self.ServerIPInput.setReadOnly(False)
+        self.UsernameBox.setReadOnly(False)
+        self.ServerBox.setReadOnly(False)
         self.ConnectButton.setEnabled(True)
 
-    def AppendUsers(self, users):
-        if (username in str(users)):
-            users.remove(username)
-
-        
-        self.ConnectedUsersTable.append("\n".join(users))
 
 class ClientThread(threading.Thread):
 
@@ -201,27 +172,30 @@ class ClientThread(threading.Thread):
         self.window = window
 
     def run(self):
-        global objSocket
+        global objSocket, KEY
         objSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        time.sleep(0.1)
         while (True):
             try:
-                objSocket.connect((ServerIP, PORT))
-                objSocket.send(bytes(f"{username}", "utf-8"))
-                self.window.Connected()
+                objSocket.connect((server, PORT))
             except:
                 continue
             else:
                 break
 
+        KEY = objSocket.recv(1024)
+        objSocket.send(Fernet(KEY).encrypt(bytes(username, "utf-8")))
+
+        self.window.ServerStatusLabel.setText("Connected")
+        self.window.ServerStatusLabel.setStyleSheet("color: green")
+
         while (True):
             try:
-                data = str(objSocket.recv(buffer), "utf-8")
+                data = Fernet(KEY).decrypt(objSocket.recv(BUFFER))
                 if not data:
-                    raise Exception
+                    raise socket.error
 
-                self.window.ReceivedMessage(data.split("|")[0])
+                self.window.ReceivedMessage(str(data, "utf-8"))
             except:
                 self.window.Disconnected()
                 objSocket.close(); objSocket = None
@@ -229,8 +203,35 @@ class ClientThread(threading.Thread):
 
 if __name__ == "__main__":
     application = QApplication([])
-    with open("GUI-Styles.css", "r") as StyleFile:
-        application.setStyleSheet(StyleFile.read())
-
+    application.setStyleSheet("""
+    #StatusLabel, #UsernameLabel, #ServerLabel {
+        color: grey;
+        font-size: 19px;
+    }
+    #ServerStatusLabel {
+        color: red;
+        font-size: 19px;
+    }
+    #UsernameBox, #ServerBox {
+        color: green;
+        font-weight: bold;
+        height: 20%;
+        width: 80%;
+    }
+    #ConnectButton, #DisconnectButton, #ClearButton {
+        color: blue;
+        height: 15%;
+        width: 60%;
+    }
+    #EncryptionLabel {
+        color: green;
+        font-weight: bold;
+    }
+    #SendButton {
+        color: white;
+        font-size: 15px;
+    }
+    #ChatBox { font-weight: bold; }
+    """)
     window = ChatWindow()
     exit(application.exec_())
